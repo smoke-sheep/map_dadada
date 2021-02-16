c785625293f0b7dcac56517261a50213a3a073bc
smoke-sheep <vladya.lizunoff@yandex.ru>
2021-02-16 21:51:41 +0700

    dadada

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import sys
 class MapWidget(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-
-        self.operate_map = Map((-71.300048, -14.408039), 0.5, "sat")
+        self.map_file = "map.png"
+        self.operate_map = Map([37.530887, 55.703118], 0.02, "map")
         uic.loadUi("design.ui", self)
         self.initUi()
 
@@ -22,27 +22,33 @@ class MapWidget(QtWidgets.QMainWindow):
         #self.update_picture()
 
     def update_picture(self):
-        self.map_file = "map.png"
+        self.map_image.setPixmap(QPixmap("Wait_img.jpg"))
         with open(self.map_file, "wb") as file:
             file.write(self.operate_map.get_picture())
-        self.map_image.setPixmap(QPixmap(self.map_file))
+        self.pixmap = QPixmap(self.map_file)
+        self.map_image.setPixmap(self.pixmap)
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
             self.close()
         elif event.key() == Qt.Key_PageUp:
-            pass
+            self.operate_map.zoom("zoom_in")
+            self.update_picture()
         elif event.key() == Qt.Key_PageDown:
-            pass
+            self.operate_map.zoom("zoom_away")
+            self.update_picture()
         elif event.key() == Qt.Key_Down:
-            #self.operate_map.move("down")
+            self.operate_map.move("down")
             self.update_picture()
         elif event.key() == Qt.Key_Up:
             self.operate_map.move("up")
+            self.update_picture()
         elif event.key() == Qt.Key_Left:
             self.operate_map.move("left")
+            self.update_picture()
         elif event.key() == Qt.Key_Right:
             self.operate_map.move("right")
+            self.update_picture()
         else:
             print(event.key())
 
--- main_map.py
+++ main_map.py
@@ -3,40 +3,52 @@ import requests, sys, os, io
 
 class Map:
     def __init__(self, ll, spn, l):
-        self.ll = ll
+        self.ll = [ll[1], ll[0]]
         self.spn = spn
         self.l = l
         self.api_server = "http://static-maps.yandex.ru/1.x/"
         self.pt = None
 
     def get_picture(self):
+        # map_params = {
+        #     "ll": self.ll,
+        #     "spn": f"{self.spn},{self.spn}",
+        #     "l": self.l,
+        #     # "pt": self.pt
+        #     }
+        print(self.ll, self.spn, self.l)
         map_params = {
-            "ll": self.ll,
-            "spn": self.spn,
-            "l": self.l,
-            "pt": self.pt
-            }
+            'll': f"{self.ll[0]},{self.ll[1]}",
+            'l': self.l,
+            'spn': f"{self.spn},{self.spn}",
+            'pt': self.pt
+        }
+
         response = requests.get(self.api_server, params=map_params)
-        # img = io.BytesIO(response.content)
         return response.content
 
     def make_pt(self, pt_ll):
         self.pt = pt_ll
 
     def move(self, direct):
-        if direct == 'left':
-            self.ll[0] -= 0.1 * self.spn
-        elif direct == 'right':
-            self.ll[0] += 0.1 * self.spn
-        elif direct == 'up':
-            self.ll[1] += 0.1 * self.spn
-        elif direct =='down':
-            self.ll[1] -= 0.1 * self.spn
-    
-    def zoom(self, spn, direct):
+        try:
+            print(direct)
+            if direct == 'left':
+                self.ll[0] -= 0.1 * self.spn
+            elif direct == 'right':
+                self.ll[0] += 0.1 * self.spn
+            elif direct == 'up':
+                self.ll[1] += 0.1 * self.spn
+            elif direct == 'down':
+                self.ll[1] -= 0.1 * self.spn
+        except Exception as e:
+            print(e)
+
+    def zoom(self, direct):
         if direct == 'zoom_in':
-            self.spn = spn + 0.1
+            self.spn *= 0.1
         elif direct == 'zoom_away':
-            self.spn = spn - 0.1
+            self.spn /= 0.1
 
-
+    def change_l(self, new_l):
+        self.l = new_l
