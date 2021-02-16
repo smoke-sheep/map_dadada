a541e834bb41a02a89bb0187de6184460fd9c55f
DG020 <55313680+DG020@users.noreply.github.com>
2021-02-16 17:23:30 +0700

    зум

--- main.py
+++ main.py
@@ -22,15 +22,11 @@ class Map:
 
     def make_pt(self, pt_ll):
         self.pt = pt_ll
-
-    def move(self, direct):
-        if direct == 'left':
-            self.ll[0] -= 0.1 * self.spn
-        elif direct == 'right':
-            self.ll[0] += 0.1 * self.spn
-        elif direct == 'up':
-            self.ll[1] += 0.1 * self.spn
-        elif direct =='down':
-            self.ll[1] -= 0.1 * self.spn
+    
+    def zoom(self, spn, direct):
+        if direct == 'zoom_in':
+            self.spn = spn + 0.1
+        elif direct == 'zoom_away':
+            self.spn = spn - 0.1
 
 
